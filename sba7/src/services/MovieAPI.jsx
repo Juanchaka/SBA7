ade89fc176076352ae512c28b75b94b8a19e9c0a
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-10-03 20:45:38 -0500

    Added netlify config file and edited to add https vs http -removed typescript residuals from package.json

--- sba7/src/services/MovieAPI.jsx
+++ sba7/src/services/MovieAPI.jsx
@@ -3,7 +3,7 @@ const API_KEY = import.meta.env.VITE_APIKEY;
 function SearchMovies (searchTerm) {
   return new Promise((resolve, reject) => {
     const searchRequest = new XMLHttpRequest();
-    searchRequest.open('GET', `http://www.omdbapi.com/?apikey=${API_KEY}&s=${searchTerm}`, true);
+    searchRequest.open('GET', `https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchTerm}`, true);
     
     searchRequest.onload = function() {
       if (searchRequest.status === 200) {
